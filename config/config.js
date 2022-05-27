e20691358b5b13a1af4ec44ba8b0f85fc0c96112
Rutu Patel <patelrutu1203@gmail.com>
2022-05-26 21:22:37 -0400

    Addee JWT secret key to config

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 const config = {
 	port: 3000,
 	connectionString: "mongodb+srv://rutu2608:Freshnest016@cluster0.vnnz5.mongodb.net/SkeletonApp?retryWrites=true&w=majority",
+	jwtSecret: "my-32-character-ultra-secure-and-ultra-long-secret"
 }
 
 export default config
