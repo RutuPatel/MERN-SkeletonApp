74f2c8a72afb555191d62e1a37ca2dc578eec9cd
Rutu Patel <patelrutu1203@gmail.com>
2022-05-26 20:38:58 -0400

    userSchema is Updated with some new Property

--- models/user.models.js
+++ models/user.models.js
@@ -6,6 +6,20 @@ const userSchema = new mongoose.Schema({
 		trim: true,
 		required: "name is required",
 	},
+	email: {
+		type: String,
+		trim: true,
+		required: "email is required",
+	},
+	password: {
+		type: String,
+		trim: true,
+		required: "password is required",
+	}
 })
+
+userSchema.method = {
+
+}
 
 export default mongoose.model("User", userSchema)
